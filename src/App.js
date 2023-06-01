2ceb2db636c7627f76b1886900acd77e69e758d1
nicolofabris <nicolo.fabriss@gmail.com>
2023-06-01 14:08:26 +0200

    create routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import ForgotPassword from './pages/ForgotPassword';
+import Home from './pages/Home'
+import Offers from './pages/Offers';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/sign-in" element={<SignIn/>}/>
+        <Route path="/sign-up" element={<SignUp/>}/>
+        <Route path="/forgot-password" element={<ForgotPassword/>}/>
+        <Route path="/offers" element={<Offers/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
