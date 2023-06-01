bad0d2763c1b89bd79bc1170bd6643db41d5628d
nicolofabris <nicolo.fabriss@gmail.com>
2023-06-01 14:42:32 +0200

    create header

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Header from './components/Header';
 import ForgotPassword from './pages/ForgotPassword';
 import Home from './pages/Home'
 import Offers from './pages/Offers';
@@ -10,6 +11,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <BrowserRouter>
+      <Header/>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/profile" element={<Profile/>}/>
