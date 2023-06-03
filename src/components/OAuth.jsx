6b211c2e7e5c532445c9132d405c9d9e593e8ed6
nicolofabris <nicolo.fabriss@gmail.com>
2023-06-03 11:01:02 +0200

    complete auth function

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,8 +1,38 @@
+import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth'
+import { doc, getDoc, serverTimestamp, setDoc } from 'firebase/firestore'
 import {FcGoogle} from 'react-icons/fc'
+import { toast } from 'react-toastify'
+import { db } from '../firebase'
+import { useNavigate } from 'react-router-dom'
 
 const OAuth = () => {
+  const navigate = useNavigate()
+
+  async function onGoogleClick() {
+    try {
+      const auth = getAuth()
+      const provider = new GoogleAuthProvider()
+      const result = await signInWithPopup(auth, provider)
+      const user = result.user
+
+      const docRef = doc(db, 'users', user.uid)
+      const docSnap = await getDoc(docRef)
+
+      if(!docSnap.exists) {
+        await setDoc(docRef, {
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp()
+        })
+      }
+      navigate('/')
+
+    } catch (error) {
+      toast.error('could not auth')
+    }
+  }
   return (
-    <button>
+    <button type='button' onClick={onGoogleClick}>
         <FcGoogle/>
         Continue with Google
     </button>
